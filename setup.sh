2f64bfb91437ab7295ebb6060587eb3229677bb9
Kyle Bader <kyle.bader@gmail.com>
2017-05-22 13:22:36 -0700

    Extract files

--- setup.sh
+++ setup.sh
@@ -6,3 +6,11 @@ curl ${mirror}/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz -o hadoop-2.7.3.ta
 curl ${mirror}/hive/hive-2.1.1/apache-hive-2.1.1-bin.tar.gz -o apache-hive-2.1.1-bin.tar.gz
 curl ${mirror}/spark/spark-2.1.1/spark-2.1.1-bin-hadoop2.7.tgz -o spark-2.1.1-bin-hadoop2.7.tgz
 curl ${mirror}/kafka/0.10.2.1/kafka_2.10-0.10.2.1.tgz -o kafka_2.10-0.10.2.1.tgz
+
+sudo mkdir /hadoop
+sudo mkdir /hadoop/{hive,spark,kafka}
+
+sudo tar xvzf /tmp/hadoop-2.7.3.tar.gz -C /hadoop
+sudo tar xvzf /tmp/apache-hive-2.1.1-bin.tar.gz -C /hadoop/hive
+sudo tar xvzf /tmp/spark-2.1.1-bin-hadoop2.7.tgz -C /hadoop/spark
+sudo tar xvzf /tmp/kafka_2.10-0.10.2.1.tgz -C /hadoop/kafka
